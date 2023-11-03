4dfeeb38d410f1b0835f588c26858fa5c4c31a56
Robotechnic <robotechnic.developeur@gmail.com>
2023-11-03 20:01:44 +0100

    refactor main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,10 @@
 <script lang="ts">
-	import Modal from '$lib/components/modal.svelte';
+  import RuleBar from './RuleBar.svelte';
+
 	import Rule from '$lib/components/rule.svelte';
 	import { grammar } from '$lib/grammar';
 
-	let dialog : Modal;
 </script>
 
-<nav class="productionRules">
-	<h2>Production rules</h2>
-	<div class="productionRules__container">
-		{#each $grammar.productions as [name, _]}
-			<Rule
-				{name}
-				rules={$grammar.productions.get(name)}
-				on:input={(e) => grammar.setProduction(name, e.detail)}
-			/>
-		{/each}
-	</div>
-	<button class="productionRules__addButton" on:click={() => dialog.open()}>Add production rule</button>
-	<Modal
-		title="Add production rule"
-		message="Enter the name of the new production rule"
-		bind:this={dialog}
-		on:ok={(e) => grammar.setProduction(e.detail, [""])}
-	/>
-</nav>
+<RuleBar/>
 
-<style lang="scss">
-	.productionRules {
-		&__container {
-			display: flex;
-			flex-direction: column;
-			gap: 1rem;
-		}
-
-		&__addButton {
-			@include primary-button;
-			margin-top: 1rem;
-		}
-	}
-</style>
