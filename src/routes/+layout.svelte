aa528a167de25bc06cfd40e4163828409999ddd9
Robotechnic <robotechnic.developeur@gmail.com>
2023-11-04 11:32:30 +0100

    fix main page layout to take entire screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,18 +11,23 @@
 </section>
 
 <style lang="scss">
+	$header-height: 7rem;
 	header {
 		background-color: $primary-color;
 		color: $primary-text-color;
 		padding: 1rem;
 		text-align: center;
-		height: 5rem;
+		height: $header-height;
 	}
 
 	section {
 		padding: 1rem;
 		width: 100vw;
 		box-sizing: border-box;
+		display: grid;
+		grid-template-columns: auto 1fr;
+		gap: .5em;
+		height: calc(100vh - $header-height);
 	}
 
 </style>
